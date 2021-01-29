aac54352b79eeeebe060e64fae010f4e46659c32
LeeJinKyu <jklee050@gmail.com>
2021-01-29 14:44:29 +0900

    Initial Commit

---
+++ YulliniOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YulliniOS
+//
+//  Created by yullin on 2021/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
