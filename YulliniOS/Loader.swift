5c1173eb33a37f86186a22968db34660d17725d8
LeeJinKyu <jklee050@gmail.com>
2021-01-29 17:59:30 +0900

    webview

---
+++ YulliniOS/Loader.swift
@@ -0,0 +1,8 @@
+//
+//  Loader.swift
+//  YulliniOS
+//
+//  Created by yullin on 2021/01/29.
+//
+
+import Foundation
---
+++ YulliniOS/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  YulliniOS
+//
+//  Created by yullin on 2021/01/29.
+//
+
+import Foundation
